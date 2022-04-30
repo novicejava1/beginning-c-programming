eb2e411dce95e897cccf997066fd273dad30c48e
novicejava1 <sudhirbhoga@gmail.com>
2022-05-01 01:12:11 +0530

    Program to display more variables

---
+++ Chapter2_First-steps-in-programming/bridesandbrothers.c
@@ -0,0 +1,11 @@
+/* Program to dispaly brides and brothers */
+#include <stdio.h>
+
+int main(void)
+{
+	int brothers = 7;
+	int brides = 7;
+
+	printf("%d brides for %d brothers\n", brides, brothers);
+	return 0;
+}
