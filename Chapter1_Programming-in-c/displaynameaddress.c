04da052764a873915187a7132ed30e8bf0390538
novicejava1 <sudhirbhoga@gmail.com>
2022-05-01 00:31:09 +0530

    Program to display name and address

---
+++ Chapter1_Programming-in-c/displaynameaddress.c
@@ -0,0 +1,9 @@
+/* Program to display name and address */
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Name:\nnovicejava1\n\n");
+	printf("Address:\n22 Python Street,\nC - Block,\nDocker Yard\n");
+	return 0;
+}
