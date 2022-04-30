9c82ef5c52be5cd3d54e6674e7432e74f747ae42
novicejava1 <sudhirbhoga@gmail.com>
2022-05-01 01:08:31 +0530

    Program to display salary stored in a variable

---
+++ Chapter2_First-steps-in-programming/displaysalary.c
@@ -0,0 +1,9 @@
+/* Program to display salary stored in a variable */
+#include <stdio.h>
+
+int main(void)
+{
+	int salary = 1000;													// Declare and Define a variable which allocated memory
+	printf("The salary of concerned persion is : Rs. %d\n", salary);
+	return 0;
+}
