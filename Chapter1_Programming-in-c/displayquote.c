d1b8f0ff3c5736d2a4871671ef1bf5188b794e63
novicejava1 <sudhirbhoga@gmail.com>
2022-04-30 20:16:41 +0530

    Program to display Quote of the day with escape sequence

---
+++ Chapter1_Programming-in-c/displayquote.c
@@ -0,0 +1,8 @@
+/*  Program to display Quote of the day with escape sequence */
+#include <stdio.h>
+
+int main(void)
+{
+	printf("\"Imagination is more important than knowledge!!\"\n");
+	return 0;
+}
