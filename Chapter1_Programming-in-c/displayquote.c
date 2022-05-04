fc2ab7ec20360010561432e5964e5d13f00cdc9e
novicejava1 <sudhirbhoga@gmail.com>
2022-05-04 17:35:02 +0530

    Display a quote with escape sequence characters

--- Chapter1_Programming-in-c/displayquote.c
+++ Chapter1_Programming-in-c/displayquote.c
@@ -1,8 +1,8 @@
 /*  Program to display Quote of the day with escape sequence */
-#include <stdio.h>
+#include <stdio.h>															// This is preprocessor directive
 
-int main(void)
-{
-	printf("\"Imagination is more important than knowledge!!\"\n");
-	return 0;
-}
+int main(void)																// This identifies the function main()
+{																			// This marks the beginning of main()
+	printf("\"Imagination is more important than knowledge!!\"\n");			// This prints the quote onto display screen
+	return 0;																// This returns control to OS
+}																			// This marks end of main()
