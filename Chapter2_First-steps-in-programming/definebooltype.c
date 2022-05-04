cfe0796e2c07b82bbc30c65c5d57b654fa9ee781
novicejava1 <sudhirbhoga@gmail.com>
2022-05-04 18:08:33 +0530

    Defining a boolean type

---
+++ Chapter2_First-steps-in-programming/definebooltype.c
@@ -0,0 +1,13 @@
+/* Defining a bool type */
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void)
+{
+	bool ok = true;
+	bool notok = false;
+	printf("What is ok : %d \n", ok);
+	printf("What is notok : %d \n", notok);
+
+	return 0;
+}
