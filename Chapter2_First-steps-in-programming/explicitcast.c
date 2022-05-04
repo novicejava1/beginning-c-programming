f6a834ddcf34ccc3b1f280bf8fa7a435a708b9f1
novicejava1 <sudhirbhoga@gmail.com>
2022-05-04 17:34:03 +0530

    explicit casting example

---
+++ Chapter2_First-steps-in-programming/explicitcast.c
@@ -0,0 +1,10 @@
+/* Example to explicity cast a variable type */
+#include <stdio.h>
+
+int a = 10;
+int b = 20;
+
+int main(void)
+{
+	printf("Divide a by b : %3.5f \n", (float)a/b);		// Explicity casting type of a to float
+}
