eb08a45dc9cc66ee0f651508654f9ff4688d0bbf
novicejava1 <sudhirbhoga@gmail.com>
2022-05-04 08:53:06 +0530

    Divide plank into equal pieces

---
+++ Chapter2_First-steps-in-programming/divideaplank.c
@@ -0,0 +1,12 @@
+/* Divide a Plank to equal parts */
+#include <stdio.h>
+
+int main(void)
+{
+	float plank_length = 10.0f;
+	float piece_count = 4.0f;
+	float piece_length = 0.0f;
+
+	piece_length = plank_length/piece_count;
+	printf("The plank of %f can be cut into %f pieces of length %f\n", plank_length, piece_count, piece_length);
+}
