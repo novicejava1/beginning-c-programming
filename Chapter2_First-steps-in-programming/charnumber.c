c150cbbeab58871755213a57918058ca4f086197
novicejava1 <sudhirbhoga@gmail.com>
2022-05-04 17:06:27 +0530

    character number examples

---
+++ Chapter2_First-steps-in-programming/charnumber.c
@@ -0,0 +1,20 @@
+/* Character and numbers example */
+#include <stdio.h>
+
+int main(void)
+{
+	char first = 'T';
+	char second = 63;
+
+	printf("The first example as a letter is : %c \n", first);
+	printf("The first example as a number is : %d \n", first);
+	printf("The second example as a letter is : %c \n", second);
+	printf("The second example as a number is : %d \n", second);
+
+	printf("Hexa decimal value of first letter is : %x \n", first);
+	printf("Hexa decimal value of second letter is : %x \n", second);
+
+
+	return 0;
+}
+
