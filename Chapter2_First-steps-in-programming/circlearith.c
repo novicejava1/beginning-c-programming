f0f34ae41a375a1e790020009f672dcc90d1c4a7
novicejava1 <sudhirbhoga@gmail.com>
2022-05-04 09:15:19 +0530

    calculate circumference and area of circle

---
+++ Chapter2_First-steps-in-programming/circlearith.c
@@ -0,0 +1,23 @@
+/* Calculate circumference and area of circle */
+#include <stdio.h>
+
+int main(void)
+{
+	float radius = 0.0f;
+	float diameter = 0.0f;
+	float cirumference = 0.0f;
+	float area = 0.0f;
+	float Pi = 3.14159265f;
+
+	printf("Input the diameter of the table\n");
+	scanf("%f", &diameter);
+
+	radius = diameter/2.0f;
+	cirumference = 2.0f*Pi*radius;
+	area = Pi*radius*radius;
+
+	printf("\n\nThe cirumference of table is %f\n", cirumference);
+	printf("\n\nThe area of the table is %f\n", area);
+
+	return 0;
+}
