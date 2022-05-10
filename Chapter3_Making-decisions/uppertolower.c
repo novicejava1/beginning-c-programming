3deef3ccb0908e820da42df2698dc68da9439d43
novicejava1 <sudhirbhoga@gmail.com>
2022-05-10 16:49:45 +0530

    convert upper to lower version 2

--- Chapter3_Making-decisions/uppertolower.c
+++ Chapter3_Making-decisions/uppertolower.c
@@ -9,14 +9,25 @@ int main(void)
 	scanf("%c", &mychar);
 	
 	if ( mychar >= 'A')
+	{
 		if (mychar <= 'Z')
 		{
 			lower_mychar = mychar + 32;
 			printf("Lower case of mychar is %c \n", lower_mychar);
 		}
 		else
-			printf("I need a uppercase letter.. \n");
-
+		{
+			if (mychar >= 'a')
+			{
+				if (mychar <= 'z')
+					printf("Try using shift key! I want a capital letter. \n");
+			}
+			else
+				printf("You didn't entered a letter.. \n");
+		}
+	}
+	else
+		printf("You didn't entered a letter.. \n");
 	return 0;
 
 }
