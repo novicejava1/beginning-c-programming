029210f3c4c25862bd3041bfd7d6cec737450438
novicejava1 <sudhirbhoga@gmail.com>
2022-05-11 10:52:42 +0530

    Examples on bitwise operators

--- Chapter3_Making-decisions/otherbitwiseop.c
+++ Chapter3_Making-decisions/otherbitwiseop.c
@@ -12,8 +12,15 @@ int main(void)
 	int result = value << shiftcount;
 	printf("Shift left by %d of %d is %d \n", shiftcount, value, result); 	// Left shift operator
 
+	unsigned int value1 = 65372U;
+	int shiftcount1 = 2;
+	unsigned int result1 = value1 >> shiftcount1;
+	printf("Shift Right by %d of %d is %d \n", shiftcount1, value1, result1);	// Right shift with unsinged integer
 
-
+	signed int value2 = -164;
+	int shiftcount2 = 2;
+	signed int result2 = value2 >> shiftcount2;
+	printf("Shift Right by %d of %d is %d \n", shiftcount2, value2, result2);	// Right shift with signed integer
 
 	return 0;
 }
