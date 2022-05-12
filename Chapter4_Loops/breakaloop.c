2cbb1f35c374877a44934ef2735eef66973f8853
novicejava1 <sudhirbhoga@gmail.com>
2022-05-12 11:26:27 +0530

    Example to break a loop

---
+++ Chapter4_Loops/breakaloop.c
@@ -0,0 +1,15 @@
+/* Example to demonstrate loop break */
+#include <stdio.h>
+#include <ctype.h>
+
+int main(void)
+{
+	char answer = 0;
+	for(;;)
+	{
+		printf("Do you want to enter some more (y/n) : ");
+		scanf("%c", &answer);
+		if(tolower(answer) == 'n')
+			break;
+	}
+}
