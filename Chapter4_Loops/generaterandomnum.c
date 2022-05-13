ecb91465f4224d035ab95b5dd4ecfaa381bba675
novicejava1 <sudhirbhoga@gmail.com>
2022-05-13 13:22:26 +0530

    Generate a random number

--- Chapter4_Loops/generaterandomnum.c
+++ Chapter4_Loops/generaterandomnum.c
@@ -6,12 +6,14 @@
 int main(void)
 {
 	srand(time(NULL));		// Initialize sequence with a particular seed
-	int chosen = 0;
-	chosen = rand();
-	
-	printf("Generated randon number is : %d \n", chosen);
+	int random1, random2, random3 = 0;
+	random1 = rand();
+	random2 = rand();
+	random3 = rand();
 
-
+	printf("Generated randon number is : %d \n", random1);
+	printf("Generated random number is : %d \n", random2);
+	printf("Generated random number is : %d \n", random3);
 
 	return 0;
 }
