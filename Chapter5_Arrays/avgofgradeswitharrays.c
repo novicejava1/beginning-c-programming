1e912bccd323bf51f91cff0c83e3519bd2d8cefe
novicejava1 <sudhirbhoga@gmail.com>
2022-05-16 16:38:28 +0530

    Example average of grades with arrays

---
+++ Chapter5_Arrays/avgofgradeswitharrays.c
@@ -0,0 +1,23 @@
+/* Example average of grades with arrays */
+#include <stdio.h>
+
+int main(void)
+{
+	int grades[10];
+	unsigned int count = 10;
+	long sum = 0L;
+	float average = 0.0f;
+
+	for(unsigned int i = 0; i < count; ++i)
+	{
+		printf("%2u> ", i + 1);
+		scanf("%d", &grades[i]);
+
+		sum += grades[i];
+	}
+
+	average = (float)sum/count;
+	printf("Average of 10 grades is : %.2f \n", average);
+
+	return 0;
+}
