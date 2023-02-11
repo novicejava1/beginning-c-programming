ec156c3feee3c20dd35e94c57a25148c6a4fb3b1
novicejava1 <sudhirbhoga@gmail.com>
2023-02-11 14:43:40 +0530

    C examples

---
+++ Chapter1_Programming-in-c/javatutorial.c
@@ -0,0 +1,6 @@
+#include<stdio.h> // define the header file  
+void main()   // define the main function  
+{  
+    printf("Welcome to C");  // print the statement.  
+    return 0;
+}  
---
+++ Chapter1_Programming-in-c/multiline.c
@@ -0,0 +1,7 @@
+/* Multiline C Program */
+
+#include <stdio.h>
+int main(void)
+{
+    printf("This is a multiline string.\nC is Great\n");    
+}
