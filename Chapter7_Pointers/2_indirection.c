1bf89ded6ca58ca099b6e4ab2a3022879c4f93bb
novicejava1 <sudhirbhoga@gmail.com>
2023-02-23 18:38:38 +0530

    C Pointers

--- Chapter7_Pointers/2_indirection.c
+++ Chapter7_Pointers/2_indirection.c
@@ -4,15 +4,22 @@
 int main(void)
 {
     int i = 3;
-    int *j;
+    int *j = NULL;
+    int result = 0;
 
     j = &i;
 
-    printf("Address of i is %u \n", &i);
-    printf("Address of i is %u \n", j);
+    printf("Address of i is %p \n", &i);
+    printf("Address of i is %p \n", j);
 
     printf("Value of i is %d \n", i);
     printf("Value of i is %d \n", *j);
+
+    printf("Address of pointer *j : %p\n", (void*)&j);
+    printf("Size of pointer : %zd bytes\n", sizeof(j));
+
+    result = *j + 5;
+    printf("Result of adding 5 to pointer *j : %d\n", result);
 
     return 0;
 }
