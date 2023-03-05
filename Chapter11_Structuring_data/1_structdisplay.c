ceba7cf67e1316915d2ba6e975445d82b0859bb7
novicejava1 <sudhirbhoga@gmail.com>
2023-03-05 14:00:33 +0530

    C Structuring Data

--- Chapter11_Structuring_data/1_structdisplay.c
+++ Chapter11_Structuring_data/1_structdisplay.c
@@ -1,6 +1,8 @@
 /* Example to Declare, Initialize and Display a Strcture */
 # include <stdio.h>
 
+typedef struct Horse Horse;// Define Horse as a type name
+
 // Declare a Structure of Type Horse
 struct Horse
 {
@@ -13,12 +15,23 @@ struct Horse
 
 int main(void)
 {
-    struct Horse dobbin = { 24, 17,"Dobbin", "Trigger", "Flossie" };        // Initialize a Structure of Type Horse with variable dobbin and their values
+    Horse dobbin = { 24, 17,"Dobbin", "Trigger", "Flossie" };        // Initialize a Structure of Type Horse with variable dobbin and their values
+    Horse trigger = {
+        .height = 15, .age = 30, .name = "Trigger", .mother = "Wesson", .father = "Smith"   // Initialize using memeber variable names, order is not important
+        };
     printf("Name : %s\n", dobbin.name);                                     // Display name
     printf("Age : %d\n", dobbin.age);                                       // Display age
     printf("Height : %d\n", dobbin.height);                                 // Display Height
     printf("Father : %s\n", dobbin.father);                                 // Display Father
     printf("Mother : %s\n", dobbin.mother);                                 // Display Mother
+
+    printf("\n");
+    
+    printf("Name : %s\n", trigger.name);                                     // Display name
+    printf("Age : %d\n", trigger.age);                                       // Display age
+    printf("Height : %d\n", trigger.height);                                 // Display Height
+    printf("Father : %s\n", trigger.father);                                 // Display Father
+    printf("Mother : %s\n", trigger.mother);                                 // Display Mother
 
     return 0;
 }
---
+++ Chapter11_Structuring_data/2_readstruc.c
@@ -0,0 +1,44 @@
+/* Example Readinga Strucuture type data */
+#include <stdio.h>
+
+typedef struct Horse Horse;
+
+typedef struct Horse Horse;// Define Horse as a type name
+
+// Declare a Structure of Type Horse
+struct Horse
+{
+    int age;
+    int height;
+    char name[20];
+    char father[20];
+    char mother[20];
+};
+
+int main(void)
+{
+    Horse trigger;
+
+    printf("Enter the name of Horse: ");
+    scanf("%s", &trigger.name);
+
+    printf("Enter the age of Horse: ");
+    scanf("%d", &trigger.age);
+
+    printf("Enter the height of Horse: ");
+    scanf("%d", &trigger.height);
+
+    printf("Enter the father name: ");
+    scanf("%s", &trigger.father);
+
+    printf("Enter the mother name: ");
+    scanf("%s", &trigger.mother);   
+
+    printf("Name : %s\n", trigger.name);                                     // Display name
+    printf("Age : %d\n", trigger.age);                                       // Display age
+    printf("Height : %d\n", trigger.height);                                 // Display Height
+    printf("Father : %s\n", trigger.father);                                 // Display Father
+    printf("Mother : %s\n", trigger.mother);                                 // Display Mother
+
+    return 0;
+}
