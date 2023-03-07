265c02203495eb66075b537549f44ee984f5e15e
novicejava1 <sudhirbhoga@gmail.com>
2023-03-07 13:23:39 +0530

    C Files

---
+++ Chapter12_Working_with_files/4_formattedoutput.c
@@ -0,0 +1,24 @@
+/* Example to demonstrate formatted output to a file */
+#include <stdio.h>
+
+int main(void)
+{
+    char *filename = "4_formattedoutput.txt";
+    FILE *pfile = NULL;
+    int num1 = 1234, num2 = 4567;
+    float pi = 3.1416f;
+
+    pfile = fopen(filename, "w");
+    if(!pfile)
+    {
+        printf("Error opening file for writing. Program terminated.\n");
+        exit(1);
+    }
+
+    if(0 > fprintf(pfile, "%12d%12d%14f", num1, num2, pi))
+        printf("Failed to write the file.\n");
+
+    fclose(pfile);
+    pfile = NULL;
+    //remove(filename);             // Uncomment this to remove the file                   
+}
